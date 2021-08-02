f8b8ae48d1cea9f82de1f6166e91e63515938a3c
Nick Leon <nicolas7@vt.edu>
2021-08-02 12:31:45 -0400

    add licenses and corresponding badges

--- Develop/index.js
+++ Develop/index.js
@@ -28,22 +28,46 @@ inquirer.prompt([
       type: 'list',
       message: 'What is your license?',
       name: 'license',
-      choices: ['MIT', 'GNU GPLv3 ', 'Apache License 2.0', 'ISC'],
+      choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0', 'ISC'],
     },
   ])
   .then((data) => {
     fs.writeFile('README.md', 
     `hey bitch ${data.name}! dont you know only losers know ${data.stack}!
-    but hey nice license`
+    but hey nice license badge`
     , (err) =>
     err ? console.log(err) : console.log('Success!')
     );
     if(data.license==='MIT'){
     fs.appendFile('README.md', 
-    `test`
+    `
+    <img src="https://img.shields.io/badge/MIT-license-green">`
     , (err) =>
     err ? console.log(err) : console.log('Success!')
     );}
+    if(data.license==='GNU GPLv3'){
+      fs.appendFile('README.md', 
+      `
+      <img src="https://img.shields.io/badge/GNU%20GPLv3-license-blue">`
+      , (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );}
+    if(data.license==='Apache License 2.0'){
+      fs.appendFile('README.md', 
+      `
+      <img src="https://img.shields.io/badge/Apache%20License%202.0-license-red">`
+      , (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );}
+    if(data.license==='ISC'){
+      fs.appendFile('README.md', 
+      `
+      <img src="https://img.shields.io/badge/ISC-license-purple">`
+      , (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );}
+    
+    
   });
   
 
--- Develop/readme.md
+++ Develop/readme.md
@@ -1,2 +1,3 @@
-hey bitch nick! dont you know only losers know HTML!
-    but hey nice license
+hey bitch nick! dont you know only losers know HTML,CSS,JavaScript,MySQL!
+    but hey nice license badge
+      <img src="https://img.shields.io/badge/Apache%20License%202.0-license-red">
